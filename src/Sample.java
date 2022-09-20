788e01fa8b433d477214403f3b3c3a0c051a8acb
Sampleproject <amejia9149@bths.edu>
2022-09-20 18:17:57 -0400

    added favorite food print statement

--- src/Sample.java
+++ src/Sample.java
@@ -2,5 +2,6 @@ public class Sample {
     public static void main(String[] args){
         System.out.println("Andy Mejia");
         System.out.println("The dark knight");
+        System.out.println("Favorite food is empanadas");
     }
 }
